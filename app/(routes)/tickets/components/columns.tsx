6853b09d6f97062e867cf91fd0a2705102cbac3e
Maria <mariale171001@gmail.com>
2023-09-19 01:07:50 +0200

    feat: pagination, sorting, filtering, search

--- app/(routes)/tickets/components/columns.tsx
+++ app/(routes)/tickets/components/columns.tsx
@@ -2,6 +2,11 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 import { Ticket } from "../data/schema"
+import { RowActions } from "./data-table-row-actions"
+import { issues, priorities, statuses } from "../data/data"
+import { DataTableColumnHeader } from "./data-table-column-header"
+import { Badge } from "@/components/ui/badge"
+
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -9,11 +14,7 @@ import { Ticket } from "../data/schema"
 export const columns: ColumnDef<Ticket>[] = [
   {
     accessorKey: "id",
-    header: "Id",
-  },
-  {
-    accessorKey: "issue",
-    header: "Issue",
+    header: "Ticket",
   },
   {
     accessorKey: "customer",
@@ -21,11 +22,48 @@ export const columns: ColumnDef<Ticket>[] = [
   },
   {
     accessorKey: "status",
-    header: "Status",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Status" />
+    ),
+    cell: ({ row }) => {
+      const status = statuses.find(
+        (status) => status.value === row.getValue("status")
+      )
+
+      if (!status) {
+        return null;
+      }
+
+      return (
+        <div className="flex w-[100px] items-center">
+          {status.icon && (
+            <status.icon className="mr-2 h-4 w-4 text-muted-foreground" />
+          )}
+          <span>{status.label}</span>
+        </div>
+      )
+    },
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id))
+    },
   },
   {
     accessorKey: "subject",
-    header: "Subject",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Subject" />
+    ),
+    cell: ({ row }) => {
+      const issue = issues.find((issue) => issue.value === row.original.issue)
+
+      return (
+        <div className="flex space-x-2">
+          {issue && <Badge variant="outline">{issue.label}</Badge>}
+          <span className="max-w-[500px] truncate font-medium">
+            {row.getValue("subject")}
+          </span>
+        </div>
+      )
+    },
   },
   {
     accessorKey: "tech",
@@ -42,5 +80,36 @@ export const columns: ColumnDef<Ticket>[] = [
   {
     accessorKey: "last_updated",
     header: "Last Updated",
+  },
+  {
+    accessorKey: "priority",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Priority" />
+    ),
+    cell: ({ row }) => {
+      const priority = priorities.find(
+        (priority) => priority.value === row.getValue("priority")
+      )
+
+      if (!priority) {
+        return null
+      }
+
+      return (
+        <div className="flex items-center">
+          {priority.icon && (
+            <priority.icon className="mr-2 h-4 w-4 text-muted-foreground" />
+          )}
+          <span>{priority.label}</span>
+        </div>
+      )
+    },
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id))
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <RowActions<Ticket> data={row.original}/>,
   },
 ]
