72f6d688f7ba59bb0d8e79f19e62f887c16a8593
Maria <mariale171001@gmail.com>
2023-10-02 12:19:47 +0200

    feat: persist api store

--- hooks/use-api-store.tsx
+++ hooks/use-api-store.tsx
@@ -1,28 +1,56 @@
 "use client"
 
 import { create } from 'zustand';
+import { persist, createJSONStorage } from "zustand/middleware"; 
+
+// https://docs.pmnd.rs/zustand/integrations/persisting-store-data
 
 interface Credentials {
   subdomain: string;
   apiKey: string;
 }
 
-interface useApiStore {
+interface User {
+  isAdmin: boolean;
+  email: string;
+  name: string;
+}
+
+interface apiStore {
+  user: User;
   credentials: Credentials;
   isLogged: boolean;
   isTestUser: boolean;
+  setUser: (user: User) => void;
+  setIsTestUser: (isTestUser: boolean) => void;
   setIsLogged: (isLogged: boolean) => void;
   setCredentials: (credentials: Credentials) => void;
+  logout: () => void;
 }
 
-export const useApiStore = create<useApiStore>((set) => ({
-  credentials: { subdomain: '', apiKey: '' },
-  isLogged: false,
-  isTestUser: false,
-  setIsLogged: (isLogged: boolean) => set({ isLogged }),
-  setCredentials: (credentials: Credentials) => set({ credentials }),
-}));
-
-// Access subdomain and apiKey from the Zustand store
-export const getSubdomain = () => useApiStore.getState().credentials.subdomain;
-export const getApiKey = () => useApiStore.getState().credentials.apiKey;
+export const useApiStore = create(
+  persist<apiStore>((set, get) => ({
+    user: { isAdmin: false, email: '', name: '' },
+    credentials: { subdomain: '', apiKey: '' },
+    isLogged: false,
+    isTestUser: false,
+    setIsTestUser: (isTestUser) => set({ isTestUser }),
+    setIsLogged: (isLogged) => set({ isLogged }),
+    setUser: (user) => set({ user }),
+    setCredentials: (credentials) => set({ credentials }),
+    logout: () => {
+      set({
+        isLogged: false,
+        isTestUser: false,
+        user: {
+          isAdmin: false,
+          email: '',
+          name: ''
+        }
+      });
+    }
+  }), {
+    name: 'api-storage', 
+    storage: createJSONStorage(() => localStorage),
+  })
+);
